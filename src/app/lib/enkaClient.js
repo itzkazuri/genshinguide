234bfee5b2c6d35f358800893a14a5df51511f6c
kadekwibutzy <kadekjuli0604@protonmail.com>
2025-04-21 23:09:16 +0800

    fix the bug

--- src/app/lib/enkaClient.js
+++ src/app/lib/enkaClient.js
@@ -1,13 +1,18 @@
 import { EnkaClient } from "enka-network-api";
 
-const enka = new EnkaClient({ cacheDirectory: "./cache" });
+const enka = new EnkaClient({
+  cacheDirectory: process.env.NODE_ENV === "development" ? "./cache" : undefined,
+  shouldCacheAssets: process.env.NODE_ENV === "development",
+});
 
 let enkaAssetsFetched = false;
 
 export async function fetchAssetsIfNeeded() {
   if (!enkaAssetsFetched) {
     console.log("Fetching Genshin data (first time only)...");
+
     await enka.cachedAssetsManager.fetchAllContents();
+
     enkaAssetsFetched = true;
     console.log("Genshin data fetched successfully");
   }
